9f7564d02daf19d6d4f54b4ce58633ec59ccf9c6
Root <Root@HOME>
2022-07-20 00:31:09 +0300

    Добавьте файлы проекта.

---
+++ Storehouse/Model/Employeer.cs
@@ -0,0 +1,54 @@
+﻿namespace Storehouse
+{
+    public partial class Employeer
+    {
+        public string FIO_Short
+        {
+            get
+            {
+                return LastName + " "+
+                    (!string.IsNullOrEmpty(FirstName) ? (FirstName[0].ToString().ToUpper()+". ") : "")+
+                    (!string.IsNullOrEmpty(MiddleName) ? (MiddleName[0].ToString().ToUpper()+".") : "");
+            }
+        }
+        public string FIO_Full
+        {
+            get
+            {
+                return LastName + " " + FirstName+" " + MiddleName;
+            }
+        }
+
+        public string RoleName
+        {
+            get
+            {
+                var role = string.Empty;
+                switch (Role)
+                {
+                    case 0:
+                    {
+                        role = "Сотрудник";
+                        break;
+                    }
+                    case 1:
+                    {
+                        role = "Руководитель";
+                        break;
+                    }
+                    case 2:
+                    {
+                        role = "МОЛ";
+                        break;
+                    }
+                    case 3:
+                        {
+                            role = "Обычный сотрудник";
+                            break;
+                        }
+                }
+                return role;
+            }
+        }
+    }
+}
